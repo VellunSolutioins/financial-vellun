c68354ef94ace8c88da9c96709e2a26574a1c564
Bruno Faboci <brunofaboci@gmail.com>
2026-06-27 16:59:05 -0300

    create payment base models | seeds

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,4 +3,5 @@ export * from './schemas/user';
 export * from './schemas/transaction';
 export * from './schemas/category';
 export * from './schemas/account';
+export * from './schemas/billing';
 export * from './types/api';
